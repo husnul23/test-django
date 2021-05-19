023f09a494e62608b0b8f9ac43309c45ed2afa82
husnul23 <fadhilah.acsl@gmail.com>
2021-05-19 15:07:41 +0700

    initial commit

---
+++ testdb/models.py
@@ -0,0 +1,9 @@
+from os import name
+from django.db import models
+
+# Create your models here.
+
+class Teacher(models.Model):
+
+    name = models.CharField(max_length=80)
+    age = models.IntegerField()
