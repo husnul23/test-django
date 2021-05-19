f5d0fc18eeb6cec0f5ee5ca222fcc913eb30dbf0
husnul23 <fadhilah.acsl@gmail.com>
2021-05-19 15:29:38 +0700

    add serializers class

---
+++ testdb/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework_json_api import serializers
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = '__all__'
